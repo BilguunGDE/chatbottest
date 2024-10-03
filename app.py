a8a02b73c627f12cd2ff5259c49891cc418dfd8e
BilguunGDE <bilguun.js@gmail.com>
2024-10-03 23:54:19 +0600

    Update

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def test():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000) #Choose your preferred port
+    app.run(debug=True, host='0.0.0.0', port=5000) #Choose your preferred portt
